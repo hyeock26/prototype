e9f6e48c3e9bd853d1fee1d02f3effe5c0442fd6
rmfchqhdms <user@example.com>
2025-06-14 16:12:25 +0900

    프로토타입 웹 애플리케이션 초기 설정

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>프로토타입 웹 애플리케이션</h1>
+        <p>환영합니다!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
